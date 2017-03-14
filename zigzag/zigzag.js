824c321d4685c46f1cbd4c294ba07b5ea41425c8
Aaron Min <min.aaron.min@gmail.com>
2017-03-14 12:26:30 -0700

    Finish more efficient version of original

--- zigzag/zigzag.js
+++ zigzag/zigzag.js
@@ -2,7 +2,45 @@
  * @param {string} s
  * @param {number} numRows
  * @return {string}
+ *
+ * P     I     N
+ * A   L S   I G
+ * Y A   H R
+ * P     I
+ *
+ * P       H
+ * A     S I
+ * Y   I   R 
+ * P L     I G
+ * A       N
+ *
+ * P   A   H   N
+ * A P L A I H G
+ * N   Y   I   R
  */
+
+var convertEfficient = function(s, numRows) {
+  if (numRows === 1) return s;
+
+  var str = '';
+  var colDistance = numRows * 2 - 2;
+  
+
+  for (var i = 0; i < numRows; ++i) {
+    for (var colWidth = i; colWidth < s.length; colWidth += colDistance) { 
+      str += s[colWidth];  
+      
+      // add diagonals only if not first or last row
+      var offset = (numRows - i - 1) * 2;
+      if (i !== 0 && i !== numRows - 1 && colWidth + offset < s.length) {
+        str += s[colWidth + offset];
+      } 
+    }
+  }
+
+  return str;
+};
+
 var convert = function(s, numRows) {
   // go to bottom while adding letters
   // when you hit bottom start zigzagging til you reach top
