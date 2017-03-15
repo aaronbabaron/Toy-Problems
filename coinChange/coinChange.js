ae24f96b2437efc4169c147236a17513aa8353cd
Aaron Min <min.aaron.min@gmail.com>
2017-03-15 16:36:21 -0700

    Increase efficiency of function

--- coinChange/coinChange.js
+++ coinChange/coinChange.js
@@ -10,27 +10,28 @@
 // recursively iterate through the coins and create decision tree with each possible subtraction of coins and the next coin after
 
 // remove front coin from  
-var coinChange = function(coins, amount, count) {
-  var isFirst = false;
-  if (count === undefined) isFirst = true;
-
+var coinChange = function(coins, amount, count, fewestCoins) {
   count = count || 0; 
+  fewestCoins = fewestCoins || Infinity;
+  var isFirst = count === undefined ? true : false;
+
   var numTimes = amount / coins[coins.length - 1];
-  var fewestCoins = Infinity;
   var completedRun;
 
   if (numTimes === 0 && amount > 0 && coins.length === 1) {
-    return Infinity;
+    return fewestCoins;
   }
 
   for (var i = 0; i <= numTimes; ++i) {
     var amt = amount - coins[coins.length - 1] * i;
     var currentCoins = count + i;
 
+    if (currentCoins > fewestCoins) return fewestCoins;
+
     if (amt === 0 && currentCoins < fewestCoins) {
       fewestCoins = currentCoins; 
     } else if (amt > 0) {
-      completedRun = coinChange(coins.slice(0, coins.length - 1), amt, currentCoins); 
+      completedRun = coinChange(coins.slice(0, coins.length - 1), amt, currentCoins, fewestCoins); 
 
       if (completedRun < fewestCoins) {
         fewestCoins = completedRun;
